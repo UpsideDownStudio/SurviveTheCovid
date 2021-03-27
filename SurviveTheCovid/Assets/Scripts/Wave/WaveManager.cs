765127f9b4456a53439e6cd8c8c97c030e5d27e0
B3rl1 <kirill.skv.2000@mail.ru>
2021-03-27 21:03:16 +0300

    Добавил систему спавна врагов
    Проработана система спавна легкой волны

---
+++ SurviveTheCovid/Assets/Scripts/Wave/WaveManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveManager : MonoBehaviour
+{
+	public static int CountOfEnemy;
+
+	public Text textOfWaveTimer;
+	public Text textOfTimeNextWave;
+	public Text textOfWaveCountEnemy;
+
+	public EnemySpawner enemySpawner;
+	public List<Transform> spawnPointsList;
+
+	private float _waveTimer;
+	private float _timeNextWave;
+
+	public void Start()
+	{
+		enemySpawner.SpawnEnemy(1, spawnPointsList);
+	}
+}
